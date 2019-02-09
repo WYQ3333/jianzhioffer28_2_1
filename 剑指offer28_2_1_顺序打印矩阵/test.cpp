3b2287ce4b2f0e9d429e95673bc9f8cc6aebeeee
浅忆梦微凉 <3538922590@qq.com>
2019-02-09 20:11:41 +0800

    第二种方法

--- 剑指offer28_2_1_顺序打印矩阵/test.cpp
+++ 剑指offer28_2_1_顺序打印矩阵/test.cpp
@@ -4,33 +4,59 @@
 using namespace std;
 
 
-/*解题思路：顺时针打印就是按圈数循环打印，一圈包含两行或者两列，在打印的时候会出现某一圈中只包含一行，要判断从左向右打印和从右向左打印的时候是否会出现重复打印，同样只包含一列时，要判断从上向下打印和从下向上打印的时候是否会出现重复打印的情况*/
 class Solution {
 public:
 	vector<int> printMatrix(vector<vector<int> > matrix) {
 		vector<int>res;
-		res.clear();
-		int row = matrix.size();//行数
-		int collor = matrix[0].size();//列数
-		//计算打印的圈数
-		int circle = ((row<collor ? row : collor) - 1) / 2 + 1;//圈数
-		for (int i = 0; i<circle; i++){
-			//从左向右打印
-			for (int j = i; j<collor - i; j++)
+		int n = matrix.size();
+		int m = matrix[0].size();
+		int layer = (min(m, n) - 1) / 2 + 1;
+		for (int i = 0; i<layer; i++){
+			for (int j = i; j<m - i; j++){
 				res.push_back(matrix[i][j]);
-			//从上往下的每一列数据 
-			for (int k = i + 1; k<row - i; k++)
-				res.push_back(matrix[k][collor - 1 - i]);
-			//判断是否会重复打印(从右向左的每行数据) 
-			for (int m = collor - i - 2; (m >= i) && (row - i - 1 != i); m--)
-				res.push_back(matrix[row - i - 1][m]);
-			//判断是否会重复打印(从下往上的每一列数据)
-			for (int n = row - i - 2; (n>i) && (collor - i - 1 != i); n--)
-				res.push_back(matrix[n][i]);
+			}
+			for (int k = i + 1; k<n - i; k++){
+				res.push_back(matrix[k][m - i - 1]);
+			}
+			for (int p = m - i - 2; (p>i - 1) && (n - i - 1 != i); p--){
+				res.push_back(matrix[n - i - 1][p]);
+			}
+			for (int q = n - i - 2; (q>i) && (m - i - 1 != i); q--){
+				res.push_back(matrix[q][i]);
+			}
 		}
 		return res;
+
 	}
 };
+//
+///*解题思路：顺时针打印就是按圈数循环打印，一圈包含两行或者两列，在打印的时候会出现某一圈中只包含一行，要判断从左向右打印和从右向左打印的时候是否会出现重复打印，同样只包含一列时，要判断从上向下打印和从下向上打印的时候是否会出现重复打印的情况*/
+//class Solution {
+//public:
+//	vector<int> printMatrix(vector<vector<int> > matrix) {
+//		vector<int>res;
+//		res.clear();
+//		int row = matrix.size();//行数
+//		int collor = matrix[0].size();//列数
+//		//计算打印的圈数
+//		int circle = ((row<collor ? row : collor) - 1) / 2 + 1;//圈数
+//		for (int i = 0; i<circle; i++){
+//			//从左向右打印
+//			for (int j = i; j<collor - i; j++)
+//				res.push_back(matrix[i][j]);
+//			//从上往下的每一列数据 
+//			for (int k = i + 1; k<row - i; k++)
+//				res.push_back(matrix[k][collor - 1 - i]);
+//			//判断是否会重复打印(从右向左的每行数据) 
+//			for (int m = collor - i - 2; (m >= i) && (row - i - 1 != i); m--)
+//				res.push_back(matrix[row - i - 1][m]);
+//			//判断是否会重复打印(从下往上的每一列数据)
+//			for (int n = row - i - 2; (n>i) && (collor - i - 1 != i); n--)
+//				res.push_back(matrix[n][i]);
+//		}
+//		return res;
+//	}
+//};
 
 void TestFunc(){
 	vector<vector<int>> array = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }
